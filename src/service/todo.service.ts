a4d8b113fe1384f2a6b86d8f6068797d3a1fe65c
yuxin <tiantony53@163.com>
2026-03-23 15:37:12 +0800

    todo完善

--- src/service/todo.service.ts
+++ src/service/todo.service.ts
@@ -1,5 +1,5 @@
 import { prisma } from '#/db'
-import type { CreateTodoInput } from '#/types/todo'
+import type { CreateTodoInput, UpdateTodoInput } from '#/types/todo'
 
 export const TodoService = {
   async getUserTodos(userId: string) {
@@ -12,6 +12,19 @@ export const TodoService = {
   async createTodo(userId: string, data: CreateTodoInput) {
     return prisma.todo.create({
       data: { ...data, userId },
+    })
+  },
+
+  async updateTodo(id: number, userId: string, data: UpdateTodoInput) {
+    return prisma.todo.update({
+      where: { id: id, userId: userId },
+      data: { ...data },
+    })
+  },
+
+  async deleteTodo(id: number, userId: string) {
+    return prisma.todo.delete({
+      where: { id: id, userId: userId },
     })
   },
 }
--- src/types/todo.ts
+++ src/types/todo.ts
@@ -1,8 +1,13 @@
-import z from "zod"
+import z from 'zod'
 
 export const CreateTodoSchema = z.object({
   title: z.string().min(1, '标题不能为空').max(100),
   context: z.string().optional(),
 })
 
+export const UpdateTodoSchema = CreateTodoSchema.partial().extend({
+  done: z.boolean().optional(),
+})
+
 export type CreateTodoInput = z.infer<typeof CreateTodoSchema>
+export type UpdateTodoInput = z.infer<typeof UpdateTodoSchema>
