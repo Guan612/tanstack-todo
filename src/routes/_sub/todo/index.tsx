82e37d504e9289847e7d53ec3656ec1022c732d9
yuxin <tiantony53@163.com>
2026-03-28 17:18:09 +0800

    集成Todo页面路由并添加鉴权守卫
    Todo路由接入 authBefore 鉴权守卫，集成数据查询和拆分后的组件，
    完成完整的增删改查、搜索筛选功能。

--- src/routes/_sub/todo/index.tsx
+++ src/routes/_sub/todo/index.tsx
@@ -1,11 +1,50 @@
+import { authBefore } from '#/lib/auth-before'
+import { TodoPageView } from '#/components/todo/TodoPageView'
+import type { TodoItem } from '#/components/todo/types'
+import { useTodoPage } from '#/hooks/todo/useTodoPage'
 import { createPageHead } from '#/lib/page-head'
+import { getTodoFn } from '#/serverfn/todo/todo.serverfn'
+import { useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_sub/todo/')({
+  beforeLoad: authBefore,
   head: createPageHead('代办事项'),
   component: RouteComponent,
 })
 
-function RouteComponent() {
-  return <div>Hello "/_sub/todo/"!</div>
+export function RouteComponent() {
+  const { data } = useSuspenseQuery({
+    queryKey: ['todos'],
+    queryFn: () => getTodoFn(),
+  })
+
+  return <TodoView todos={(data ?? []) as TodoItem[]} />
+}
+
+export function TodoView({ todos }: { todos: TodoItem[] }) {
+  const todoPage = useTodoPage(todos)
+
+  return (
+    <TodoPageView
+      filteredTodos={todoPage.filteredTodos}
+      keyword={todoPage.keyword}
+      filter={todoPage.filter}
+      createForm={todoPage.createForm}
+      editingId={todoPage.editingId}
+      editingValues={todoPage.editingValues}
+      onCreateTitleChange={todoPage.setCreateTitle}
+      onCreateDescriptionChange={todoPage.setCreateDescription}
+      onCreateSubmit={todoPage.handleCreate}
+      onKeywordChange={todoPage.setKeyword}
+      onFilterChange={todoPage.setFilter}
+      onStartEditing={todoPage.startEditing}
+      onEditingTitleChange={todoPage.setEditingTitle}
+      onEditingDescriptionChange={todoPage.setEditingDescription}
+      onSave={todoPage.handleSave}
+      onCancelEditing={todoPage.cancelEditing}
+      onToggle={todoPage.toggleTodo}
+      onDelete={todoPage.deleteTodo}
+    />
+  )
 }
